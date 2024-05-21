77ced6fcbee9279fb2807e171d51263a5509c810
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-21 18:50:04 +0000

    Deploying to gh-pages from @ Nexs12/goit-js-hw-10@4ac0747eeeee9739f695791b7f28eb77e4f21851 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-cd4b7665.js";o.error({title:"Error",message:"Illegal operation",overlayColor:"rgba(181, 27, 27, 0.6)",position:"topRight"});
+//# sourceMappingURL=commonHelpers.js.map
