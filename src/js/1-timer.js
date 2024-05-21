4ac0747eeeee9739f695791b7f28eb77e4f21851
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-05-21 21:49:48 +0300

    start

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,12 @@
 import '../css/styles.css';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+iziToast.error({
+    title: 'Error',
+    message: 'Illegal operation',
+    overlayColor: 'rgba(181, 27, 27, 0.6)',
+    position: 'topRight'
+});
